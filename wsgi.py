17df88b5aa47df089550f358a4da42ec204b3426
Prasad Engineer <engineerprasad21@gmail.com>
2025-04-13 02:34:48 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,22 @@
 """
-WSGI config for VidyAI project.
+WSGI config for vidyai project.
 
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
+It exposes the WSGI callable as a module-level variable named `application`.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
 import sys
-from pathlib import Path
 
-# Add the VidyAI directory to the path
-current_dir = Path(__file__).resolve().parent
-vidyai_dir = current_dir / 'VidyAI'
-sys.path.append(str(vidyai_dir))
+# Add the parent directory to the sys.path
+current_path = os.path.dirname(os.path.abspath(_file_))
+parent_path = os.path.dirname(current_path)
+sys.path.append(parent_path)
 
-# Set the Django settings module
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vidyai.settings')
 
-# Import the application from the VidyAI/vidyai/wsgi.py file
-from vidyai.wsgi import application 
+application = get_wsgi_application()
