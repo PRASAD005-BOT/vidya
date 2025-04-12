2972f8d822a47dd5e37a5c267d3a42cd74ef9638
Prasad Engineer <engineerprasad21@gmail.com>
2025-04-13 02:49:00 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,19 +1,13 @@
 """
 WSGI config for vidyai project.
 
-It exposes the WSGI callable as a module-level variable named `application`.
+It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
-# Add the parent directory to the sys.path
-current_path = os.path.dirname(os.path.abspath(_file_))
-parent_path = os.path.dirname(current_path)
-sys.path.append(parent_path)
 
 from django.core.wsgi import get_wsgi_application
 
